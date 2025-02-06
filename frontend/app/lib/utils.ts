a0e5268700f52c8f0a166ebea9d9b2f5250e38bc
Simarjeet <sjthecoder@gmail.com>
2025-02-07 04:06:49 +0530

    Refactor makeRequest function to accept custom headers and improve documentation

--- frontend/app/lib/utils.ts
+++ frontend/app/lib/utils.ts
@@ -5,10 +5,19 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+
+export interface RequestOptions extends RequestInit {
+  headers?: Record<string, string>;
+}
+
 /**
- * Makes an HTTP request to the specified endpoint.
+ * Makes an HTTP request.
+ *
+ * @param endpoint - The relative path of the endpoint.
+ * @param options - Optional request configuration. No need to pass credentials and Content-Type headers.
+ * @returns A promise resolving to the response.
  */
-export function makeRequest(endpoint: string, options?: RequestInit): Promise<Response> {
+export function makeRequest(endpoint: string, options?: RequestOptions): Promise<Response> {
   const BASE_URL = "http://127.0.0.1:8000/api/v1";
   const normalizedEndpoint = endpoint.startsWith("/") ? endpoint : `/${endpoint}`;
   const URL = `${BASE_URL}${normalizedEndpoint}`;
