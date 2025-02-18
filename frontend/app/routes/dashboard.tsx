e3511bd0ed0bca1f7e908c9855a7a26959d4016a
Simarjeet <sjthecoder@gmail.com>
2025-02-18 21:57:07 +0530

    Add minimum sidebar design, update config docker to use watch instead of volume bind

--- frontend/app/routes/dashboard.tsx
+++ frontend/app/routes/dashboard.tsx
@@ -1,16 +1,33 @@
-import { Outlet } from "@remix-run/react";
-import { DashboardSidebar } from "@/components/common/DashboardSidebar";
+import { Outlet, useLoaderData } from "@remix-run/react";
+import { LoaderFunction } from "@remix-run/node";
+import { DashboardSidebar } from "~/components/common/DashboardSidebar";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { useRequireAuthentication } from "~/hooks/useRequireAuthentication";
+import { Separator } from "@/components/ui/separator";
+
+export const loader: LoaderFunction = async ({ request }) => {
+    const cookieHeader = request.headers.get("Cookie");
+    const sidebarState = cookieHeader?.includes("sidebar_state=true") ?? false;
+
+    return new Response(JSON.stringify({ defaultOpen: sidebarState }), {
+        headers: { "Content-Type": "application/json" },
+    });
+};
 
 export default function DashboardLayout() {
     useRequireAuthentication();
+    const { defaultOpen } = useLoaderData<typeof loader>();
 
     return (
-        <SidebarProvider>
+        <SidebarProvider defaultOpen={defaultOpen}>
             <DashboardSidebar />
-            <main>
-                <SidebarTrigger />
+            <main className="w-full">
+                <div className="flex w-full shrink-0 items-center gap-4 border-b-2 p-2">
+                    <SidebarTrigger />
+                    <Separator orientation="vertical" className="h-5" />
+                    {/* TODO: Add breadcrumbs */}
+                    <p>Bruh</p>
+                </div>
                 <Outlet />
             </main>
         </SidebarProvider>
